8484baef6a11b42497c8ee6330f7e41db7ef8704
Anderson Oliveira <andervilo@gmail.com>
2025-05-12 14:26:28 -0300

    Add Credito entity model to the domain layer
    Introduces the Credito class as a JPA entity with fields representing key credit attributes such as numeroCredito, valorIssqn, and baseCalculo. Includes Lombok annotations for boilerplate code reduction and JPA annotations for entity mapping. This is foundational for handling credit-related operations in the system.

---
+++ credito-ms/src/main/java/com/andervilo/creditoms/domain/model/Credito.java
@@ -0,0 +1,33 @@
+package com.andervilo.creditoms.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Credito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String numeroCredito;
+    private String numeroNfse;
+    private LocalDate dataConstituicao;
+    private BigDecimal valorIssqn;
+    private String tipoCredito;
+    private boolean simplesNacional;
+    private BigDecimal aliquota;
+    private BigDecimal valorFaturado;
+    private BigDecimal valorDeducao;
+    private BigDecimal baseCalculo;
+}
+
